1f15aa95a70124650ec8c972f05badaebf2f520d
Thomas107500 <49201909+Thomas107500@users.noreply.github.com>
2021-06-01 17:19:18 +0800

    test:Adding 3 managers

---
+++ test/colonyManager.js
@@ -0,0 +1,19 @@
+var stat = require('statManager');
+
+var colonyManager = {
+
+//1. Get necessary room data(energy count? resource count? creep count? role composition?)
+//2. evaluate room status(under attack? threat imminent? condition escalating?)
+//3. Grab job and assign them to creep
+//4. Spawn creep(if there are not enough creep, spawn them and assign them)
+//5. determine the next high level action(expand? reinforce rooms? retreat? relocate?)
+    
+    run: function() {
+        for (var room of Game.rooms) {
+            stat.getRoleCreepsByRoom(room,"harvester");
+        }
+    }
+    
+};
+
+module.exports = colonyManager;
---
+++ test/roomManager.js
@@ -0,0 +1,7 @@
+var roomManager = {
+    roomManager: function(roomName){
+        this.roomName = roomName;
+        this.room = Game.rooms.roomName;
+    }
+};
+module.exports = roomManager;
